a3b4c4b71f14948ee61c5bb374e2af53c6a313d8
caceres90 <caceresmartinezjhonalexander@gmail.com>
2025-06-20 16:41:51 -0500

    Creacion de dto y definicion de responsabilidades

---
+++ src/task/entitys/task.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from '../status/status-task';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  title: string;
+  @Column()
+  description: string;
+  @Column({ default: 'pending', type: 'enum', enum: TaskStatus })
+  status: TaskStatus;
+}
